c0e2f2b2c5515eb8bc558e7cc4e9452da8875e2d
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-11 16:18:34 -0300

    feat: get minion by id route

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -13,4 +13,15 @@ minionsRouter.get('/', (request, response) => {
   response.send(getAllFromDatabase('minions'));
 });
 
+minionsRouter.get('/:minionId', (request, response) => {
+  const { minionId } = request.params;
+  const minion = getFromDatabaseById('minions', minionId);
+
+  if (!minion) {
+    return response.status(404).send('Minion not found');
+  }
+
+  response.send(minion);
+})
+
 module.exports = minionsRouter;
