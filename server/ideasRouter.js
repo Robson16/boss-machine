816c4f826e2e4d60316d90c1acd62a87a4f0b970
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-07-14 14:25:45 -0300

    feat: get idea by id

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -13,4 +13,20 @@ ideasRouter.get('/', (request, response) => {
   response.send(getAllFromDatabase('ideas'));
 });
 
+ideasRouter.get('/:ideaId', (request, response) => {
+  const { ideaId } = request.params;
+
+  if (isNaN(ideaId)) {
+    return response.status(404).send('Invalid idea ID');
+  }
+
+  const idea = getFromDatabaseById('ideas', ideaId);
+
+  if (!idea) {
+    return response.status(404).send('Idea not found');
+  }
+
+  response.send(idea);
+});
+
 module.exports = ideasRouter;
